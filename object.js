f618633f112a1e48768a5b0b9c1bee276bcf4759
smart-xy <smartxysmart@gmail.com>
2022-04-08 14:18:26 +0100

    my first object

---
+++ object.js
@@ -0,0 +1,35 @@
+//1.. Create an Object that has properties "username" and "password". Fill those values in with strings
+var smart = {
+    username: "smart-xy",
+    password: "hint199"
+};
+console.log(smart);
+
+//2.. Create an array which contains the object you have made above and name the array "database".
+var database = [
+    {
+        smart,
+        // username: "smart-xy",
+        // password: "hint199"
+    }
+];
+console.log(database);
+
+//3.. Create an array called "newsfeed" which contains 3 objects with properties "username" and "timeline."
+var newsFeed = [
+    {
+        username: "johnny",
+        timeline: "One"
+    },
+
+    {
+        username: "Emily",
+        timeline: "Three"
+    },
+
+    {
+        username: "Jaden",
+        timeline: "Six"
+    }
+];
+console.log(newsFeed);
